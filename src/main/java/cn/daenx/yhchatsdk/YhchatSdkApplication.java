cd975ede6fe00fcb70b3074c8a9fd75da04a7941
DaenMax <1330166565@qq.com>
2023-06-12 15:17:07 +0800

    初始化项目

---
+++ src/main/java/cn/daenx/yhchatsdk/YhchatSdkApplication.java
@@ -0,0 +1,13 @@
+package cn.daenx.yhchatsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhchatSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YhchatSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/daenx/yhchatsdk/YhchatSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.daenx.yhchatsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhchatSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
