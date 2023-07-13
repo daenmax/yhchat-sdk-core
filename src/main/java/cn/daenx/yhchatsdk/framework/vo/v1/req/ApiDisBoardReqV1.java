94bf5418e6d33d816b79d868f5cb613dfdac34ce
DaenMax <1330166565@qq.com>
2023-07-13 11:23:33 +0800

    优化代码

--- src/main/java/cn/daenx/yhchatsdk/framework/vo/v1/req/ApiDisBoardReqV1.java
+++ src/main/java/cn/daenx/yhchatsdk/framework/vo/v1/req/ApiDisBoardReqV1.java
@@ -1,6 +1,5 @@
 package cn.daenx.yhchatsdk.framework.vo.v1.req;
 
-import cn.daenx.yhchatsdk.common.constant.ContentTypeConstant;
 import lombok.Data;
 
 /**
@@ -23,12 +22,6 @@ public class ApiDisBoardReqV1 {
      * 群: group
      */
     private String recvType;
-    /**
-     * 消息类型，取值如下
-     * text\markdown\html
-     */
-    private String contentType;
-    private String content;
 
     //快捷构造方法
 
@@ -38,50 +31,11 @@ public class ApiDisBoardReqV1 {
      *
      * @param recvType RecvTypeConstant.
      * @param recvId   当为USER时，填用户ID，当为GROUP时，填群ID
-     * @param content  消息内容
      * @return
      */
-    public ApiDisBoardReqV1 Text(String recvType, String recvId, String content) {
+    public ApiDisBoardReqV1 Dis(String recvType, String recvId) {
         this.recvId = recvId;
         this.recvType = recvType;
-        this.contentType = ContentTypeConstant.TEXT;
-        this.content = content;
-        return this;
-    }
-
-
-    /**
-     * Markdown消息
-     * 如果要设置全局看板，recvType和recvId请传null
-     *
-     * @param recvType RecvTypeConstant.
-     * @param recvId   当为USER时，填用户ID，当为GROUP时，填群ID
-     * @param content  md消息内容
-     * @return
-     */
-    public ApiDisBoardReqV1 Markdown(String recvType, String recvId, String content) {
-        this.recvId = recvId;
-        this.recvType = recvType;
-        this.contentType = ContentTypeConstant.MARKDOWN;
-        this.content = content;
-        return this;
-    }
-
-
-    /**
-     * Html消息
-     * 如果要设置全局看板，recvType和recvId请传null
-     *
-     * @param recvType RecvTypeConstant.
-     * @param recvId   当为USER时，填用户ID，当为GROUP时，填群ID
-     * @param content  html
-     * @return
-     */
-    public ApiDisBoardReqV1 Html(String recvType, String recvId, String content) {
-        this.recvId = recvId;
-        this.recvType = recvType;
-        this.contentType = ContentTypeConstant.HTML;
-        this.content = content;
         return this;
     }
 
